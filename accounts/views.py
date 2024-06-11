25d3bf5c5e0d704cbba13d1bef7e11e3bf7a31d1
Mirav21 <miravsavaliya2004@gmail.com>
2024-06-11 12:29:51 +0530

    handeled Cart and Checkout page fot logged in user

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
+
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 from .models import Account
 from .forms import RegistrationForm
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+import requests
 
 #Varification Email
 from django.contrib.sites.shortcuts import get_current_site
@@ -63,9 +66,60 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id = _cart_id(request))
+                is_cart_item_exist = CartItem.objects.filter(cart=cart).exists()
+                
+                if is_cart_item_exist:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    #Getting the product variations by cart-id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                        
+                    #Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                            
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+                    
+            except:
+                pass
+            
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('accounts:dashboard')
+            url = request.META.get('HTTP_REFERER')
+            
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+                
+            except:
+                return redirect('accounts:dashboard')
         
         else:
             messages.error(request, 'Invlid login credentials.')
